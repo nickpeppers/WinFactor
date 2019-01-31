f54aa0d89c5cba3bd3a5e5b16f46a8ae4e8c199b
Nick Peppers <nicholas.peppers778@topper.wku.edu>
2019-01-31 17:58:24 -0500

    Fixed keyboard type on Entry and lock orientation to portrait

--- WinFactor/WinFactor.Android/MainActivity.cs
+++ WinFactor/WinFactor.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using WinFactor.Services.Interfaces;
 
 namespace WinFactor.Droid
 {
-    [Activity(Label = "WinFactor", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "WinFactor", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
